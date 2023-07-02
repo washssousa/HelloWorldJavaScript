4731ff98c8be34ede84609d96b84625cc060ce5e
Washington Sousa <59042632+washssousa@users.noreply.github.com>
2023-07-02 18:28:23 -0300

    rename code

--- console.js
+++
@@ -1,5 +0,0 @@
-//I hey this is a comment
-console.log("Hello World")
-console.log("Hello World") // 2th time day.
-console.log("Hello World") // 2th time day two.
-console.log("Hello World") // 2th time day one.
